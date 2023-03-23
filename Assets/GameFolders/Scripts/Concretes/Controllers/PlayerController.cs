ef6f35aaae5d691c32687ae62f9e389f8a677418
Burak Sevincli <brksvncli@gmail.com>
2023-03-23 13:29:47 +0300

    Development Scene Created
    Player Controller Scripts Created

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/PlayerController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFolders.Scripts.Concretes.Controllers
+{
+    public class PlayerController : MonoBehaviour
+    {
+        
+        private void Awake()
+        {
+        }
+
+        private void FixedUpdate()
+        {
+        }
+
+        private void Update()
+        {
+        }
+    }
+}
+
---
+++ Assets/GameFolders/Scripts/Concretes/Movements/MoveWithTranslate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFolders.Scripts.Concretes.Movements
+{
+    public class MoveWithTranslate : MonoBehaviour
+    {
+        public void HorizontalMove(float horizontal)
+        {
+            
+        }
+    }
+}
+
